f35635ee63ceda5c96b39947c4a5538cdcabd361
YueHuawastaken Hua <yu3hua@gmail.com>
2024-02-12 15:54:03 +0000

    third commit

--- Shipping/data.js
+++ Shipping/data.js
@@ -1,18 +1,9 @@
-// const productId = [
-//     {
-//         "ID" : 123,
-//         "Name" : "Naruto Resin",
-//         "Value" : "400SGD",
-//     },
-//     {  "ID" : 456,
-//         "Name" : "Sasuke Resin",
-//         "Value" : "500SGD"
-//     },
-//     {   "ID" : 789,
-//          "Name" : "Gojo Resin",
-//          "Value" : "600SGD" 
-//         },
-// ]
+// We need 1) JSON BIN ID and the base JSON BIN API URL
+const BIN_ID="65ca363edc74654018a3c0fc";
+const BASE_JSON_BIN_URL="https://api.jsonbin.io/v3/b";
+// not a good idea to embed the master key in data.js
+const MASTER_KEY="$2a$10$pkQ6JeXUpcUuukmKgJyuQ.qd9cQ3gYSzeCG3WbAvXv99EwS9KpOie";
+
 function displayProducts (productId) {
     console.log ("Your Items");
     console.log ();
@@ -23,5 +14,72 @@ function displayProducts (productId) {
     }
 }
 // displayProducts(productId); 
+// function addProduct (productId, ID, Name, Value) {
+    // const newParcel = {
+    //     "ID" : ID,
+    //     "Name" : Name,
+    //     "Value" : Value,
+    //    }
+    //   productId.push(newParcel);
+// }
+ function createProduct (productId)
+ {
+    const ID = prompt("Enter the parcel ID : ");
+    const Name = prompt ("Enter Product Name :");
+    const Value = prompt ("Enter the Value :");
+    const newParcel = {
+     "ID" : ID,
+     "Name" : Name,
+     "Value" : Value,
+    }
+   productId.push(newParcel);
+ }
 
-// function createProduct 
+ function editProducts (p, productId) {
+    let wantedProduct = null;
+ for (let i of productId)
+ if (i.id == p.id) {
+    wantedProduct = i;
+    break;
+}
+const newName = prompt("Enter Your New Product Name :")
+const newValue = prompt("Enter the New Value :")
+wantedProduct.Name = newName;
+wantedProduct.Value = newValue;
+}
+
+ function deleteProducts (p, productId) {
+        // we need to find the index of the task that we want in delete in the todos array
+        let wantedIndex = null;
+        for (let d = 0; d < productId.length; d++) {
+            // console.log(productId[d])
+            // check if the id of the task that I want to delete
+            // matches the id of the task currently indicated by index `i`.
+            if (productId[d].ID == p.ID) {
+                wantedIndex = d;
+            }
+        }
+        if (wantedIndex != null) {
+            // use .splice to delete an element from array
+            // parameter 1: where to start deleting (aka which index to start deleting from)
+            // parameter2 : how many to delete
+            productId.splice(wantedIndex, 1);
+        }
+ }
+
+ async function loadProductsJSONBIN() {
+    const response = await axios.get(`${BASE_JSON_BIN_URL}/${BIN_ID}/latest`);
+    console.log(response.data);
+    return response.data.record;
+}
+
+async function saveProducts(productId) {
+    // first parameter: the URL (aka the endpoint)
+    // second parameter: new data which will overrwrite the existing one
+    // third parameter: header options (stores meta data)
+    const response = await axios.put(`${BASE_JSON_BIN_URL}/${BIN_ID}`, productId,{
+        'Content-Type':"application/json",  // inform JSON BIN API that we are sending something in JSON format
+        'X-Master-Key': MASTER_KEY, // provide the credientals to update the JSON BIN
+    })
+    return response.data;
+}
--- Shipping/script.js
+++ Shipping/script.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", async function(){
-    const filedata = await LoadProducts();
-    const productId = [];
-    LoadData(filedata,productId);
-    console.log(productId);
 
+document.addEventListener("DOMContentLoaded", async function(){
+    //const filedata = await LoadProducts();
+    const productId = await loadProductsJSONBIN();
+    //LoadData(filedata,productId);
+    console.log(productId);
+    const saveBtn = document.querySelector ("#saveButton");
+    saveBtn.addEventListener ("click", function () {
+   saveProducts (productId);
+    })
     const processBtn = document.querySelector ("#processBtn");
     processBtn.addEventListener("click", function () {
     const typeInText = document.querySelector ("#shippingId");
@@ -15,13 +19,45 @@ document.addEventListener("DOMContentLoaded", async function(){
         console.log("Invalid Shipping ID")
     }
 })
+const addButton = document.querySelector ("#addButton");
+addButton.addEventListener ("click", function () {
+//     const ID = prompt("Enter the parcel ID : ");
+//     const Name = prompt ("Enter Product Name :");
+//     const Value = prompt ("Enter the Value :");
+//     const newParcel = {
+//      "ID" : ID,
+//      "Name" : Name,
+//      "Value" : Value,
+//     }
+//    productId.push(newParcel);
+//    console.log(productId);
+    createProduct(productId);
+   renderTable (productId);
+
 });
+});
+// const prompt = require ('prompt-sync') ();
+// const addButton = document.querySelector ("#addButton");
+// addButton.addEventListener ("click", function () {
+    // const ID = prompt("Enter the parcel ID : ");
+    // const Name = prompt ("Enter Product Name :");
+    // const Value = prompt ("Enter the Value :");
+    // const newParcel = {
+    //  "ID" : ID,
+//      "Name" : Name,
+//      "Value" : Value,
+//     }
+//    productId.push(newParcel);
+
+// });
 
 
 function renderTable (productId) {
+    // console.log(productId);
     const displayItems = document.querySelector ("#displayItems")
 displayItems.style.display = "block";
 const tableProducts = document.querySelector ("#tablebody")
+tableProducts.innerHTML = " ";
 for (let p of productId) {
     const tableRow = document.createElement("tr");
     tableRow.innerHTML = `
@@ -34,12 +70,33 @@ for (let p of productId) {
 // select the edit button that is inside the productId
 const editButton = tableRow.querySelector(".edit-button");
 editButton.addEventListener("click", function(){
-    processEditAppointment(p, productId);
+    editProducts(p, productId);
+    renderTable(productId);
 })
 
 // select the delete button that is inside the productId
 tableRow.querySelector(".delete-button").addEventListener("click", function(){
-    processCancelAppointment(p, productId);
+    // console.log("Delete")
+    deleteProducts(p, productId);
+    renderTable(productId);
+
+    // function deleteTask(p, productId) {
+    //     // we need to find the index of the task that we want in delete in the todos array
+    //     let wantedIndex = null;
+    //     for (let d = 0; d < p.length; i++) {
+    //         // check if the id of the task that I want to delete
+    //         // matches the id of the task currently indicated by index `i`.
+    //         if (productId == p[d].productId) {
+    //             wantedIndex = d;
+    //         }
+    //     }
+    //     if (wantedIndex) {
+    //         // use .splice to delete an element from array
+    //         // parameter 1: where to start deleting (aka which index to start deleting from)
+    //         // parameter2 : how many to delete
+    //         todos.splice(wantedIndex, 1);
+    //     }
+    // }
 })
 
     tableProducts.appendChild (tableRow)
@@ -58,3 +115,5 @@ function LoadData(filedata,productId) {
     productId.push (f)
      }
 }
+// const addButton = document.querySelector (".addbutton");
+// addButton.addEventListener ("click", function ())
